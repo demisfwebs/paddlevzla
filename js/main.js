b0525adf26be7e4767c98f4901259508b378bbf8
demisfwebs <demisfermincontact@gmail.com>
2024-07-03 12:41:50 -0400

    full

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-//PRELOADER
-
-window.onload = function(){
-    var contenedor = document.getElementById('contenedor-carga')
-    contenedor.style.visibility = 'hidden';
-    contenedor.style.opacity = '0';}
-
-var swiper = new Swiper(".mySwiper", {
-     effect: "cube",
-      grabCursor: true,
-      loop: true,
-      centeredSlides: true,
-      slidesPerView: "1.1",
-      cubeEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-});
-
 let slider = document.querySelector(".slider-contenedor")
 let sliderIndividual = document.querySelectorAll(".ctn-slider")
 let contador = 1;
@@ -54,3 +28,30 @@ function slides(){
             contador=1;
 
         },1500)}}
+
+//PRELOADER
+
+window.onload = function(){
+    var contenedor = document.getElementById('contenedor-carga')
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';}
+
+var swiper = new Swiper(".mySwiper", {
+     effect: "cube",
+      grabCursor: true,
+      loop: true,
+      centeredSlides: true,
+      slidesPerView: "1.1",
+      cubeEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
+
